24445c12932bd5a5379e5bca8022432c9570ac02
pranav <pranav8267@gmail.com>
2025-01-17 23:19:04 +0530

    starter code

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
